8e6fcca34cdaa83a9148da865d681cba2f7570f2
Sargis Hakobyan <sargis1918@gmail.com>
2024-05-31 00:41:16 +0400

    1

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('hello welcome')
+  res.send('hello-welcome'+'i made-it!!!!!')
 })
 
 app.listen(port, () => {

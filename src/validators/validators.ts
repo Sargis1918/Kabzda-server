3a5b15131cfa9aa07892377407fba8699e2730cc
Sargis Hakobyan <sargis1918@gmail.com>
2024-07-14 18:14:59 +0400

    yty

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -3,7 +3,7 @@ import { HTTP_STATUSES } from "../utilite";
 import { NextFunction } from "express-serve-static-core";
 export const titleValidation=body("title").isLength({ min: 3, max: 10 })
 export const titleValidationResult=(req:any,res:any,next:NextFunction)=>{
-    debugger
+  
     const result = validationResult(req);
     if (!result.isEmpty()) {
       res. status(HTTP_STATUSES.bad_request_400).send({error: result.array() });
